51ac59081ea077d5a8052936d275e69f90d5f1c2
tiash-and-cats <tiash.rbm@gmail.com>
2026-01-31 23:49:46 +0600

    Modified logging in nvgif.js

--- nvgif.js
+++ nvgif.js
@@ -205,28 +205,30 @@ async function handleNVGIFImages() {
                              img[src$=".nvg2"], img[src$=".nvg3"], 
                              img[src$=".nvg4"]`).forEach(async(e) => {
     const img = new NVGIFImage(e.src);
-    console.log("Loading image:", e.src);
+    const start = Date.now();
+    console.log("nvgif: Loading image:", e.src);
     img.onload = async () => {
       e.dataset.originalSrc = e.src;
       e.src = URL.createObjectURL(await img.canvas.convertToBlob());
-      console.log("Loaded image:", e.dataset.originalSrc);
+      console.log("nvgif: Loaded image:", e.dataset.originalSrc, "in", Date.now() - start, "ms");
     };
     img.onerror = () => {
-      console.error("Failed to decode NVGIF:", e.src);
+      console.error("nvgif: Failed to decode NVGIF:", e.src, "in", Date.now() - start, "ms");
     };
   });
   document.querySelectorAll(`picture > source[srcset$=".nvg"], picture > source[srcset$=".nvg1"],
                              picture > source[srcset$=".nvg2"], picture > source[srcset$=".nvg3"], 
                              picture > source[srcset$=".nvg4"]`).forEach(async(e) => {
     const img = new NVGIFImage(e.srcset);
-    console.log("Loading image:", e.srcset);
+    const start = Date.now();
+    console.log("nvgif: Loading image:", e.srcset);
     img.onload = async () => {
       e.dataset.originalSrcset = e.srcset;
       e.srcset = URL.createObjectURL(await img.canvas.convertToBlob());
-      console.log("Loaded image:", e.dataset.originalSrcset);
+      console.log("nvgif: Loaded image:", e.dataset.originalSrcset, "in", Date.now() - start, "ms");
     };
     img.onerror = () => {
-      console.error("Failed to decode NVGIF:", e.srcset);
+      console.error("nvgif: Failed to decode NVGIF:", e.srcset, "in", Date.now() - start, "ms");
     };
   });
 }
