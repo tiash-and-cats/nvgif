3b4f96642b65e5c3e118e9d8d850efc53f76ce33
tiash-and-cats <tiash.rbm@gmail.com>
2026-01-31 14:51:11 +0600

    Made the C# demo a bit more fancy

--- csharp/NVGIFTest.cs
+++ csharp/NVGIFTest.cs
@@ -1,15 +1,40 @@
 using System;
-using System.Drawing;        // for Bitmap
-using System.Drawing.Imaging; // for ImageFormat
-using System.IO;
+using System.Drawing;
+using System.Windows.Forms;
+using NVGIF;
 
-class Program
+namespace NVGIFTest
 {
-    static void Main(string[] args)
+    class Program
     {
-        byte[] bytes = File.ReadAllBytes("../images/drawing.nvg");
-        Bitmap bmp = NVGIF.NVGIF.Decode(bytes);
-        bmp.Save("decoded.png", ImageFormat.Png);
-        Console.WriteLine("NVGIF decoded!");
+        [STAThread] // required for WinForms
+        static void Main(string[] args)
+        {
+            Application.EnableVisualStyles();
+            Application.Run(new NVGIFViewerForm());
+        }
+    }
+
+    public class NVGIFViewerForm : Form
+    {
+        private PictureBox pictureBox;
+
+        public NVGIFViewerForm()
+        {
+            this.Text = "NVGIF Viewer";
+            this.Width = 600;
+            this.Height = 600;
+
+            pictureBox = new PictureBox
+            {
+                Dock = DockStyle.Fill,
+                SizeMode = PictureBoxSizeMode.Zoom
+            };
+            this.Controls.Add(pictureBox);
+
+            byte[] bytes = System.IO.File.ReadAllBytes("../images/drawing.nvg");
+            Bitmap bmp = NVGIF.NVGIF.Decode(bytes);
+            pictureBox.Image = bmp;
+        }
     }
 }
---
+++ csharp/README.md
@@ -0,0 +1,12 @@
+# NVGIF C# demo
+
+> [!TIP]
+> As it uses WinForms, this demo must be run on Windows.  
+> Also the `.csproj` given uses .NET 10, which you might not have. Adjust it to your needs.
+
+To run the demo, do:
+``` batch
+dotnet run
+```
+
+It will run the demo and open a WinForms window which shows the demo file `images/example.nvg`.
